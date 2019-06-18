272f3cf264cded359f41775bf9a4cea09b9326d7
nocxiz <nocxiz@gmail.com>
2019-06-18 20:37:41 +0700

    Update liff-starter.js

--- liff-app/environmental-sensing/liff-starter.js
+++ liff-app/environmental-sensing/liff-starter.js
@@ -352,3 +352,34 @@ function uiToggleStateButton(pressed) {
         el.innerText = "Released";
     }
 }
+function uiToggleLoadingAnimation(isLoading) {
+    const elLoading = document.getElementById("loading-animation");
+
+    if (isLoading) {
+        // Show loading animation
+        elLoading.classList.remove("hidden");
+    } else {
+        // Hide loading animation
+        elLoading.classList.add("hidden");
+    }
+}
+
+function uiStatusError(message, showLoadingAnimation) {
+    uiToggleLoadingAnimation(showLoadingAnimation);
+
+    const elStatus = document.getElementById("status");
+    const elControls = document.getElementById("controls");
+
+    // Show status error
+    elStatus.classList.remove("success");
+    elStatus.classList.remove("inactive");
+    elStatus.classList.add("error");
+    elStatus.innerText = message;
+
+    // Hide controls
+    elControls.classList.add("hidden");
+}
+
+function makeErrorMsg(errorObj) {
+    return "Error\n" + errorObj.code + "\n" + errorObj.message;
+}
